3f1cc95325991d75ef7bacea3fdb42ce479c42e4
snghnl <viselacity@gmail.com>
2025-05-09 19:00:03 +0900

    add: add feature for basic crud

---
+++ src/lib/crud.ts
@@ -0,0 +1,44 @@
+import { db } from "./firebase";
+import {
+    doc,
+    collection,
+    addDoc,
+    getDocs,
+    getDoc,
+    updateDoc,
+    deleteDoc,
+} from "firebase/firestore";
+
+export type Task = {
+    id: string;
+    title: string;
+    description?: string;
+    dueDate: Date;
+};
+
+export const createTask = (task: Task) => {
+    const taskRef = collection(db, "tasks");
+    addDoc(taskRef, task);
+};
+
+export const readTasks = async (): Promise<Task[]> => {
+    const taskRef = collection(db, "tasks");
+    const querySnapshot = await getDocs(taskRef);
+    return querySnapshot.docs.map((doc) => doc.data() as Task);
+};
+
+export const getTaskById = async (id: string): Promise<Task> => {
+    const taskRef = doc(db, "tasks", id);
+    const querySnapshot = await getDoc(taskRef);
+    return querySnapshot.data() as Task;
+};
+
+export const updateTask = async (id: string, task: Task) => {
+    const taskRef = doc(db, "tasks", id);
+    await updateDoc(taskRef, task);
+};
+
+export const deleteTask = async (id: string) => {
+    const taskRef = doc(db, "tasks", id);
+    await deleteDoc(taskRef);
+};
